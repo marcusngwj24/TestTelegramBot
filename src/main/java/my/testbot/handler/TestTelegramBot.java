7001a8748d47c25e4ccbe868f62cc67ca6a234cb
“marcusngwj” <marcusngwj@msn.com>
2017-01-14 23:45:14 +0800

    Update handler to handle the different command

--- src/main/java/my/testbot/handler/TestTelegramBot.java
+++ src/main/java/my/testbot/handler/TestTelegramBot.java
@@ -10,7 +10,13 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+import my.testbot.common.GameManager;
+import my.testbot.game.Game;
+import my.testbot.game.NounQuestion;
+
 public class TestTelegramBot extends TelegramLongPollingBot {
+    public static GameManager gameManager = GameManager.getInstance();
+           
     @Override
     public void onUpdateReceived(Update update) {
         if (hasNewUpdate(update)) {
@@ -21,154 +27,75 @@ public class TestTelegramBot extends TelegramLongPollingBot {
     private void execute(Update update) {
         Message incomingMsg = update.getMessage();
         
+        System.out.println("Incoming Msg: " + incomingMsg);        
         SendMessage message = createMessage(incomingMsg);
 
         executeSendMessage(message);
     }
 
     private SendMessage createMessage(Message incomingMsg) {
+        long chatId = incomingMsg.getChatId();
         String incomingMsgLowerCase = incomingMsg.getText().toString().toLowerCase();
+        Game currentGame;
         
         SendMessage message = null;
-        if (incomingMsgLowerCase.contains("pig")) {
-            message = new SendMessage() 
-                                .setChatId(incomingMsg.getChatId())
-                                .setText("HAHAHA! " + incomingMsg.getFrom().getFirstName() + " is a pig");
+        
+        
+        if (incomingMsgLowerCase.contains("/start@testmycurrentbot")) {
+            if (gameManager.getGameList().containsKey(chatId) && gameManager.getGameList().get(chatId).getGameStatus()) {
+                message = new SendMessage()
+                        .setChatId(chatId)
+                        .setText("There is already an ongoing session.\nごめんなさい >.<");
+            }
+            else if (gameManager.getGameList().containsKey(chatId)) {
+                message = playGame(chatId, incomingMsgLowerCase);
+            }
+            else {
+                Game newGame = new Game();
+                newGame.startGame(chatId);
+                gameManager.addGame(chatId, newGame);
+                message = playGame(chatId, incomingMsgLowerCase);
+            }
         }
         
-        else if (incomingMsgLowerCase.equals("hi") || incomingMsgLowerCase.equals("hello")
-                || incomingMsgLowerCase.contains("hii")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText("Aye aye Captain " + incomingMsg.getFrom().getFirstName() + "!");
+        else if (incomingMsgLowerCase.contains("/stop@testmycurrentbot")) {
+            currentGame = gameManager.getGameList().get(chatId);
+            currentGame.stopGame(chatId);
+            message = new SendMessage()
+                    .setChatId(chatId)
+                    .setText("Thanks for playing");
         }
         
-        else if (incomingMsgLowerCase.contains("bye") || incomingMsgLowerCase.contains("cya")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText("Good bye " + incomingMsg.getFrom().getFirstName() + "!");
+        else if (gameManager.getGameList().containsKey(chatId) 
+                && gameManager.getGameList().get(chatId).getGameStatus() 
+                && incomingMsgLowerCase.contains("/next")) {
+            currentGame = gameManager.getGameList().get(chatId);
+            message = new SendMessage()
+                    .setChatId(chatId)
+                    .setText(currentGame.newQuestion());
         }
         
-        else if (incomingMsgLowerCase.contains("lol") || incomingMsgLowerCase.contains("haha")) {
-            message = generateReplyToLaughter(incomingMsg);
+        else if (gameManager.getGameList().containsKey(chatId) && gameManager.getGameList().get(chatId).getGameStatus()){
+            currentGame = gameManager.getGameList().get(chatId);
+            
+            if (currentGame.getCurrentNounQuestion().isCorrectAnswer(incomingMsgLowerCase)) {
+                message = new SendMessage()
+                        .setChatId(chatId)
+                        .setText("IT IS CORRECT!");
+            }
         }
-        
-        else if (incomingMsgLowerCase.contains("shut up") || incomingMsgLowerCase.contains("quiet")
-                || incomingMsgLowerCase.contains("shaddup") || incomingMsgLowerCase.contains("shuddup")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText("BB ish sad... Please don't scold me ><");
-        }
-        
-        else if (incomingMsgLowerCase.contains("thanks") || incomingMsgLowerCase.contains("tks")
-                || incomingMsgLowerCase.contains("thx") || incomingMsgLowerCase.equals("ty")
-                || incomingMsgLowerCase.contains("thank you") || incomingMsgLowerCase.contains("arigato")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText("I'm not sure if you are thanking me, but I assume you are. So you are welcome! Love you muacks!");
-        }
-        
-        else if (incomingMsgLowerCase.contains("weird")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText("Who's weird? Lai lai, come gossip!");
-        }
-        
-        else if (incomingMsgLowerCase.contains("stupid") || (incomingMsgLowerCase).contains("dumb")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText("My mummy say cannot scold people stupid. Don't scold ok?");
-        }
-        
-        else if (incomingMsgLowerCase.contains("good")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText("I see the word good, you saying me uh? So embarrassing! ");
-        }
-        
-        else if (incomingMsgLowerCase.contains("yay")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText("I feel so happy for you, " + incomingMsg.getFrom().getFirstName());
-        }
-        
-        else if (incomingMsgLowerCase.equals("die")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText("Oh my? Who died? Nuuuuuuuuuu......");
-        }
-        
-        else if (incomingMsgLowerCase.contains("/stop")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText("Why my lord... Why you want to stop...");
-        }
-        
-        else if (incomingMsgLowerCase.contains("/start")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText("Yup! I'm bored, let's play!");
-        }
-        
-        else if (incomingMsgLowerCase.contains("eygpt")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText("The place of pyramids");
-        }
-        
-        else if (incomingMsgLowerCase.contains("india")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText(incomingMsg.getFrom().getFirstName() + ", do you know there are 22 official languages in India?");
-        }
-        
-        else if (incomingMsgLowerCase.contains("japan")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText(incomingMsg.getFrom().getFirstName() + ", konnichiwa! Ganbatte!");
-        }
-        
-        else if (incomingMsgLowerCase.contains("korea")) {
-            message = new SendMessage() 
-                    .setChatId(incomingMsg.getChatId())
-                    .setText(incomingMsg.getFrom().getFirstName() + ", saranghaeyo!");
-        }
-        
-        
-        
         
         return message;
-        
     }
     
-    private SendMessage generateReplyToLaughter(Message incomingMsg) {
-        String user = incomingMsg.getFrom().getFirstName();
-        
-        String[] replyArr = { "Quiet " + user + "! Very funny izzit?!" ,
-                              "Hahahah, yeah very funny. Keep laughing " + user + ", I wont stop you" , 
-                              "Shut up" , 
-                              "Lololol" ,
-                              "Lame" ,
-                              "HAHAHA! I kennot! Please stop!", 
-                              user + ", eh, very funny harh? Dun laugh till die, I wont save you" ,
-                              "Awww your laughter is so KAWAII!!!!" ,
-                              "Shyttt!!! Im dying!!!" ,
-                              "not funny. very funny meh?" ,
-                              "Quiet la. noisy sia you" ,
-                              "Your laughter makes me fall in love w you. Muacks!" ,
-                              "Joke" ,
-                              "You look dumb. Please stop laughing"};
-        
-        Random rn = new Random();
-        int maximum = replyArr.length-1;
-        int minimum = 0;
-        int range = maximum - minimum + 1;
-        int randomNum =  rn.nextInt(range) + minimum;
-                
-                
-        return new SendMessage() 
-                .setChatId(incomingMsg.getChatId())
-                .setText(replyArr[randomNum]);
+    private SendMessage playGame(long chatId, String userInput){
+        Game currentGame = gameManager.getGameList().get(chatId);
+        currentGame.startGame(chatId);
+        String question = currentGame.newQuestion();
+        SendMessage message = new SendMessage()
+                .setChatId(chatId)
+                .setText(question);
+        return message;
     }
     
     private void executeSendMessage(SendMessage message) {
